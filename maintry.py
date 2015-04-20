5c043a9c5f2f14e641ab7c090109157823affcf5
ryangaudy <ryangaudy@yahoo.com>
2015-04-20 01:01:36 -0700

    Trying to set this up...

---
+++ maintry.py
@@ -0,0 +1,75 @@
+import pygame
+
+pygame.init()
+
+#color definitions
+
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
+
+display_width = 1200
+display_height = int(display_width * .75)
+block_size = 10 
+FPS = 20
+
+pygame.image.get_extended()
+
+icon = pygame.image.load("resources/images/gameicon.jpg")
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_icon(icon)
+pygame.display.set_caption("Adventures of Bob")
+
+gameExit = False
+
+clock = pygame.time.Clock()
+
+pygame.mixer.music.load("resources/music/gamemusic.ogg")
+pygame.mixer.music.set_volume(.5)
+pygame.mixer.music.play(loops = -1)
+
+lead_x = display_width/2
+lead_x_change = 0
+lead_y = display_height/2
+lead_y_change = 0
+
+while not gameExit:
+
+	for event in  pygame.event.get():
+		if event.type == pygame.QUIT:
+			gameExit = True
+
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				lead_x_change -= block_size
+				#lead_y_change = 0
+			elif event.key == pygame.K_RIGHT:
+				lead_x_change += block_size
+				#lead_y_change = 0
+			elif event.key == pygame.K_UP:
+				lead_y_change -= block_size
+				#lead_x_change = 0
+			elif event.key == pygame.K_DOWN:
+				lead_y_change += block_size
+				#lead_x_change = 0
+			elif event.key == pygame.K_SPACE:
+				lead_x_change = -lead_x
+				lead_y_change = -lead_y
+
+	if lead_x >= display_width or lead_x <= 0 or lead_y >= display_height or lead_y <= 0:
+		gameExit = True
+
+
+
+	lead_x += lead_x_change
+	lead_y += lead_y_change	
+
+	gameDisplay.fill(white)
+	pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,10,10])
+
+	pygame.display.update()
+	clock.tick(FPS)
+
+pygame.quit()
+quit()
---
+++ readmap.py
@@ -0,0 +1,73 @@
+import pygame
+
+pygame.image.get_extended()
+
+
+class game_map:
+
+	def __init__(self):
+		self.layers = 0 #number of total layers in this map
+		self.resolution = [] #list for the (width, height) of each tile's pixels
+		self.mapsize = [] #list for the (width,heigth) of the map's tiles
+		self.filename = "" #string for the file's location
+		self.tilesetname = "" #string for the tileset paired with this map
+		self.tiledata = []
+		
+
+	def load_map(self, filename):
+		self.filename = filename
+		map_file = open(filename)
+		raw_map = map_file.readlines()
+		map_file.close()
+
+		self.layers = int(raw_map[0])
+		self.resolution = (raw_map[1]).split(",")
+		self.mapsize = (raw_map[2]).split(",")
+		self.tilesetname = "resources/map/tiles/%s.png" % str(raw_map[4][0:len(raw_map[4])-1])
+		self.tiledata = []
+		raw_map_trim = raw_map[6:int(len(raw_map)-1)]
+		raw_map_3d = []
+		for line in raw_map_trim:
+			single_tile_data = line.split(",")
+			single_tile_data[5] = single_tile_data[5][0]
+			raw_map_3d.append(single_tile_data)
+		#print raw_map_3d
+
+
+		tile_source = pygame.image.load(self.tilesetname)
+
+		master_w = int(self.resolution[0]) * int(self.mapsize[0])
+		master_h = int(self.resolution[1]) * int(self.mapsize[1])
+
+
+		self.master_surface = pygame.Surface((master_w,master_h))
+
+		for line in raw_map_3d:
+			# line[0] = layer line [1,2] = top,left in tilefile
+			# line[3,4] = collum, row in map
+			# line[5] = z, extra flag
+			wx = int(line[3]) * int(self.resolution[1])
+			wy = int(line[4]) * int(self.resolution[1])
+			wq = int(line[1])
+			ww = int(line[2])
+			we = int(self.resolution[1])
+			wr = int(self.resolution[1])
+			self.master_surface.blit(tile_source, (wx,wy), (wq, ww, we, wr))
+
+
+		pygame.image.save(self.master_surface, "Level1.bmp")
+		print "Map Saved!"
+
+
+
+
+
+
+
+
+
+
+map_file_name = "resources/map/Level1.txt"
+
+myscene = game_map()
+myscene.load_map(map_file_name)
